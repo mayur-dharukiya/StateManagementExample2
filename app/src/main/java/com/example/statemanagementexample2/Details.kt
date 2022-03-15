de90236a6d70e9071f1f714a2503782422880e24
mayur-dharukiya <mayur@revature.com>
2022-03-15 07:48:41 -0400

    2nd commit

--- app/src/main/java/com/example/statemanagementexample2/Details.kt
+++ app/src/main/java/com/example/statemanagementexample2/Details.kt
@@ -1,24 +1,18 @@
 package com.example.statemanagementexample2
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.statemanagementexample2.ui.theme.StateManagementExample2Theme
 
-class MainActivity : ComponentActivity() {
+class Details : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -28,43 +22,22 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                           RememberState()
+                    Greeting("Android")
                 }
             }
         }
     }
 }
+
 @Composable
-fun RememberState()
-{
-   // var clickCount:Int=0
-    var clickCount by rememberSaveable {mutableStateOf(0) }
-    var context= LocalContext.current
-
-    Column{
-
-        Button(onClick={
-
-            //clickCount=clickCount+1
-            clickCount++
-        })
-        {
-
-            Text(text=""+clickCount+"Times clicked")
-        }
-
-        Button(onClick={
-
-            context.startActivity(Intent(context,Details::class.java))
-
-        })
-        {
-
-            Text(text="Go to Next Screen")
-        }
-
-    }
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
 }
 
-
-
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    StateManagementExample2Theme {
+        Greeting("Android")
+    }
+}
